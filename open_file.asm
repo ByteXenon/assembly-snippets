ca91f5c22b91150b9653ba9496869b8a38023694
ByteXenon <ddavi142@asu.edu>
2023-09-19 15:54:10 -0600

    Update open_file.asm

--- open_file.asm
+++ open_file.asm
@@ -1,13 +1,16 @@
+; This assembly code uses system interrupts to open, read and display the file from the "filename" variable
+
+; Set interrupt names
 SYS_EXIT: equ 1
 SYS_READ: equ 3
 SYS_WRITE: equ 4
 SYS_OPEN: equ 5
 
-
 section .data
     filename db './open_file.asm'
     filename_len equ $-filename
 
+; Reserve 65536 bytes for file contents 
 section .bss
     num resb 65536
 
@@ -15,24 +18,28 @@ section .text
     global _start
 
 _start:
-    mov eax, SYS_OPEN
-    mov ebx, filename
+    ; Open the file
+    mov eax, SYS_OPEN ; Move the interrupt number to eax
+    mov ebx, filename ; Move the filename pointer to ebx
     mov ecx, 0
     mov edx, 1
-    int 80h
+    int 80h ; Call the interrupt
 
-    mov ebx, eax
-    mov eax, SYS_READ
-    mov ecx, num
-    mov edx, 65536
-    int 80h
+    ; Read the file, move all contents to "num"
+    mov ebx, eax ; Move the returned "eax" register from "SYS_OPEN" to ebx for "SYS_READ" 
+    mov eax, SYS_READ ; Move the interrupt number to eax
+    mov ecx, num ; Move the file contents array pointer to "ecx"
+    mov edx, 65536 ; Max. file content size
+    int 80h ; Call the interrupt
 
-    mov eax, SYS_WRITE
-    mov ebx, 1
-    mov ecx, num
-    mov edx, 65536
-    int 80h
+    ; Output file contents to console
+    mov eax, SYS_WRITE ; Move the interrupt number to eax
+    mov ebx, 1 ; Move 1 which means "stdout"
+    mov ecx, num ; Move the file contents array pointer to "ecx", again
+    mov edx, 65536 ; Max. file content size
+    int 80h ; Call the interrupt
 
-    mov eax, SYS_EXIT
-    xor ebx, 0
-    int 80h
+    ; Exit from the program
+    mov eax, SYS_EXIT ; Move the interrupt number to eax
+    mov ebx, 0 ; Set return number to "0" (success) 
+    int 80h ; Call the interrupt
